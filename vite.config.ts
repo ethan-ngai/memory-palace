70ebec8ee385987254939cf083e54ea81b1e56c4
Ethan Ngai <engai2018@gmail.co>
2026-03-28 12:21:58 -0400

    feat: initialized project structure

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
   resolve: {
     tsconfigPaths: true,
   },
